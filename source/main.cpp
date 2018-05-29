77564f014dce0ae53a9d6239120df15c5b234d36
James Devine <j.devine@lancaster.ac.uk>
2018-05-29 16:35:01 +0100

    Added sample for cloud variables and rest services

--- source/main.cpp
+++ source/main.cpp
@@ -24,17 +24,100 @@ DEALINGS IN THE SOFTWARE.
 */
 
 #include "MicroBit.h"
+#include "DynamicType.h"
+#include "Radio.h"
+#include "WeatherService.h"
+#include "CloudVariable.h"
 
 MicroBit uBit;
+WeatherService s(uBit.radio);
+
+
+void log_num_priv(int c)
+{
+    uBit.serial.printf("%s",c);
+}
+void log_string_priv(const char * c)
+{
+    uBit.serial.printf("%s",c);
+}
+void log_string(const char * c)
+{
+    uBit.serial.printf("%s",c);
+}
+void log_num(int c)
+{
+    uBit.serial.printf("%d\r\n",c);
+}
+
+void send_get(MicroBitEvent)
+{
+    uBit.display.scroll(s.getTemperature("lancaster,uk"));
+}
+
+void send_post(MicroBitEvent)
+{
+    int ret = s.setRoomTemperature("C20",uBit.thermometer.getTemperature());
+    uBit.display.scroll(ret);
+}
+
+// int main()
+// {
+//     // Initialise the micro:bit runtime.
+//     uBit.init();
+//     uBit.radio.enable();
+
+//     uBit.serial.printf("HELLO send");
+
+//     uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, send_get);
+//     uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, send_post);
+
+//     // if (t.getString().length() > 0)
+//     //     uBit.display.scroll(t.getString());
+//     // else
+//     //     uBit.display.scroll("no data");
+//     // If main exits, there may still be other fibers running or registered event handlers etc.
+//     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
+//     // sit in the idle task forever, in a power efficient sleep.
+//     release_fiber();
+// }
+#include "CloudVariable.h"
+CloudVariable* c;
+
+int i = 0;
+
+void modify_cv(MicroBitEvent)
+{
+    *c = ManagedString(i);
+    i++;
+}
+
+void scroll_var(MicroBitEvent)
+{
+    uBit.display.scroll(c->value);
+    i++;
+}
 
 int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
+    uBit.radio.enable();
+    uBit.display.print('A');
+    uBit.serial.printf("HELLO send");
 
-    // Insert your code here!
-    uBit.display.scroll("HELLO WORLD! :)");
+    c = new CloudVariable("school","test",uBit.radio);
 
+    uBit.serial.printf("%d",c->variableNamespaceHash);
+
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, modify_cv);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, send_get);
+    uBit.messageBus.listen(RADIO_CLOUD_VARIABLE_ID, c->variableNameHash, scroll_var);
+
+    // if (t.getString().length() > 0)
+    //     uBit.display.scroll(t.getString());
+    // else
+    //     uBit.display.scroll("no data");
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
     // sit in the idle task forever, in a power efficient sleep.
